a33b1c671a1019c744db8a8f706be533fbc6fddf
Johnnyboi9907 <johnnytalexiou@icloud.com>
2025-11-19 10:04:09 -0500

    First commit

---
+++ src/WeightCalculator.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author 2483268
+ */
+public class WeightCalculator extends Application{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+    }
+    
+}
