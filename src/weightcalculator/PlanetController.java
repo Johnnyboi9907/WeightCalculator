6117f0c591ea7ef2fcb7692213638ac60156b8b5
DavidAyoubVanier <davidayoub.rwa@gmail.com>
2025-11-26 12:01:36 -0500

    Planet Return Handler

--- src/weightcalculator/PlanetController.java
+++ src/weightcalculator/PlanetController.java
@@ -1,13 +1,19 @@
 package weightcalculator;
 
+import javafx.application.Application;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
 public class PlanetController {
 
@@ -25,6 +31,9 @@ public class PlanetController {
 
     @FXML
     private Button selectBtn;
+    
+    @FXML
+    private Button returnBtn;
 
     private String[] planets;
 
@@ -36,6 +45,9 @@ public class PlanetController {
         Planet venus = new Planet("Venus", 8.87, new Image("images/Venus.jpg"));
         Planet jupiter = new Planet("Jupiter", 24.5, new Image("images/Jupiter.png"));
         Planet sun = new Planet("Sun", 275, new Image("images/Sun.jpg"));
+        
+        selectBtn.setDisable(true);
+        selectBtn.setVisible(false);
 
         // create an array of Strings containing all the names of the planets
         planets = new String[]{"Earth", "Moon", "Mars", "Venus", "Jupiter", "Sun"};
@@ -56,41 +68,68 @@ public class PlanetController {
                         nameLbl.setText(earth.getName());
                         accelerationLbl.setText("Acceleration due to gravity = " + String.valueOf(earth.getAcceleration()) + " m/s\u00B2");
                         imgView.setImage(earth.getImage());
+                        selectBtn.setDisable(false);
+                        selectBtn.setVisible(true);
                         break;
 
                     case "Moon":
                         nameLbl.setText(moon.getName());
                         accelerationLbl.setText("Acceleration due to gravity = " + String.valueOf(moon.getAcceleration()) + " m/s\u00B2");
                         imgView.setImage(moon.getImage());
+                        selectBtn.setDisable(false);
+                        selectBtn.setVisible(true);
                         break;
 
                     case "Mars":
                         nameLbl.setText(mars.getName());
                         accelerationLbl.setText("Acceleration due to gravity = " + String.valueOf(mars.getAcceleration()) + " m/s\u00B2");
                         imgView.setImage(mars.getImage());
+                        selectBtn.setDisable(false);
+                        selectBtn.setVisible(true);
                         break;
 
                     case "Venus":
                         nameLbl.setText(venus.getName());
                         accelerationLbl.setText("Acceleration due to gravity = " + String.valueOf(venus.getAcceleration()) + " m/s\u00B2");
                         imgView.setImage(venus.getImage());
+                        selectBtn.setDisable(false);
+                        selectBtn.setVisible(true);
                         break;
 
                     case "Jupiter":
                         nameLbl.setText(jupiter.getName());
                         accelerationLbl.setText("Acceleration due to gravity = " + String.valueOf(jupiter.getAcceleration()) + " m/s\u00B2");
                         imgView.setImage(jupiter.getImage());
+                        selectBtn.setDisable(false);
+                        selectBtn.setVisible(true);
                         break;
 
                     case "Sun":
                         nameLbl.setText(sun.getName());
                         accelerationLbl.setText("Acceleration due to gravity = " + String.valueOf(sun.getAcceleration()) + " m/s\u00B2");
                         imgView.setImage(sun.getImage());
+                        selectBtn.setDisable(false);
+                        selectBtn.setVisible(true);
                         break;
                 }
             }
         });
 
+    }
+    
+    @FXML
+    void handleReturn(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/weightcalculator/mainScreen.fxml"));
+            Parent root = loader.load();
+            Stage stage = (Stage) returnBtn.getScene().getWindow();
+            Scene mainScene = new Scene(root);
+            stage.setScene(mainScene);
+            
+        } catch (Exception e) {
+            
+        }
+        
     }
     
     // method for button set on action -> should return back to the main screen and confirm the planet selection (maybe add another void method to return the selected acceleration)
