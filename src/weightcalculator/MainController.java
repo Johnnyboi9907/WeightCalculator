d9cac305f6edaf27fbc279cbd277dcd271a8fc04
Johnnyboi9907 <johnnytalexiou@icloud.com>
2025-11-27 19:06:20 -0500

    run button displays weight

--- src/weightcalculator/MainController.java
+++ src/weightcalculator/MainController.java
@@ -4,7 +4,6 @@ package weightcalculator;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
  */
-
 import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -25,10 +24,6 @@ import javafx.stage.Stage;
  * @author 2483268
  */
 public class MainController {
-
-    private Stage stage;
-    private Scene scene;
-    private Parent root;
 
     @FXML
     private MenuItem clearBtn;
@@ -59,17 +54,28 @@ public class MainController {
 
     @FXML
     private MenuBar menuBar;
-    
+
     @FXML
     private ImageView imgView;
-    
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    private double weight;
+    private double mass;
+    private double acceleration;
+    private ObjectController oc;
+    private PlanetController pc;
+
     public void updateImage(Image image) {
         imgView.setImage(image);
     }
 
+
+
     @FXML
     public void initialize() {
-        
+
     }
 
     // close the program
@@ -84,10 +90,11 @@ public class MainController {
     void openObjectMenu(ActionEvent event) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("objectScreen.fxml"));
         Parent root = loader.load();
-        
+
         ObjectController oc = loader.getController();
         oc.setMainController(this);
-        
+        this.oc = oc;
+
         Stage stage = new Stage();
         //stage = (Stage) menuBar.getScene().getWindow();
         stage.setTitle("Object Menu");
@@ -103,6 +110,14 @@ public class MainController {
         stage.setTitle("Planet Menu");
         stage.setScene(new Scene(root));
         stage.show();
+    }
+
+    @FXML
+    void handleRun(ActionEvent event) {
+        mass = oc.getSelectedObject().getMass() / 1000;
+        acceleration = 9.8;
+        weight = mass * acceleration;
+        weightLbl.setText("Weight = " + String.valueOf(weight) + " kg");
     }
 
 }
